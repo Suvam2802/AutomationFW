ca09b7208513e97af84b2e8e64bd4f47d0005ca8
Suvam2802 <108133177+Suvam2802@users.noreply.github.com>
2022-09-28 09:13:31 +0530

    Add files via upload

---
+++ DDFramework/src/main/java/DDF/DDFramework/App.java
@@ -0,0 +1,13 @@
+package DDF.DDFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
